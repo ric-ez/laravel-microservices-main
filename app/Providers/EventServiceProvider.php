36e5a65d0e73682c28b570109d2cf907b4033e0b
Ric <ez@ezs-MacBook-Air.local>
2020-11-11 19:43:58 +0700

    add productcreated service

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,16 +2,13 @@
 
 namespace App\Providers;
 
-use App\Jobs\TestJob;
-use Illuminate\Auth\Events\Registered;
-use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
+use App\Jobs\ProductCreated;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
-use Illuminate\Support\Facades\Event;
 
 class EventServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        \App::bindMethod(TestJob::class . '@handle', fn($job) => $job->handle());
+        \App::bindMethod(ProductCreated::class . '@handle', fn($job) => $job->handle());
     }
 }
